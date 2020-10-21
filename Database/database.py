98712a6ee756b8518f2f1b199f6d55f6fdef9078
reydc <daniel.rey.consultas@gmail.com>
2020-10-20 23:11:19 -0300

    Agregada la clase User y dependencia, y corregido el nombre del módulo a importar

--- Database/database.py
+++ Database/database.py
@@ -1,11 +1,23 @@
-from Pony.orm import *
+from pony.orm import *
+
+import datetime
 
 db = Database()
 db.bind(provider='sqlite', filename='secretVoldemort.sqlite', create_db=True)
 
 # Agregar entidades antes del mapping
 
+# User
 
+class User(db.Entity):
+    email    = PrimaryKey(str, 100)
+    username = Required(str, 20, unique = True)
+    password = Required(bytes)
+    icon     = Required(bytes)
+    creation_date    = Required(datetime.datetime)
+    last_access_date = Required(datetime.datetime)
+    is_validated     = Required(bool)
+    active           = Required(bool)
 
 #
 
