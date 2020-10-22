464158bb08ed74ea3976ae629d1605fb5e0e8a64
reydc <daniel.rey.consultas@gmail.com>
2020-10-21 21:45:00 -0300

    User metadata para el framework

---
+++ API/Model/userMetadata.py
@@ -0,0 +1,39 @@
+# Metadata tags
+
+user_metadata_testing = [
+    {
+        "name": "User data (Testing)",
+        "description": "Get user data by email.",
+    },
+]
+
+user_metadata = [
+    {
+        "name": "Register",
+        "description": "Register user.",
+    },
+    {
+        "name": "Login",
+        "description": "Login endpoint. Returns token and session data for requests.",
+    },
+    {
+        "name": "Logout",
+        "description": "Logout endpoint. Revoke tokens and end session.",
+    },
+    {
+        "name": "Profile",
+        "description": "Return user profile data",
+    },
+    {
+        "name": "Update username",
+        "description": "Update username. Requires authentication",
+    },
+    {
+        "name": "Update password",
+        "description": "Update password. Requires authentication",
+    },
+    {
+        "name": "Update icon",
+        "description": "Update icon. Requires authentication",
+    },
+]
