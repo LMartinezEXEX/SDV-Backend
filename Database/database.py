b330e6144528c21aa048293bf43da282c13eddaa
Lautaro Martinez <lautaro.martinezexex@gmail.com>
2020-10-22 21:28:15 -0300

    Defined Turn, Vote, Player_vote and Card PonyORM entities, also defined Game and Player class to future tests and entity relations needed at the moment

--- Database/database.py
+++ Database/database.py
@@ -19,6 +19,63 @@ class User(db.Entity):
     is_validated     = Required(bool)
     active           = Required(bool)
 
+#-------------------------------------------------------------------------------
+
+class Turn(db.Entity):
+    turn_number =  PrimaryKey(int, auto=True)
+    game = Required('Game')
+    current_minister = Required('Player')
+    current_director = Required('Player')
+    last_minister = Required('Player')
+    last_director = Required('Player')
+    candidate_minister = Required('Player')
+    candidate_director = Required('Player')
+    vote = Optional('Vote')
+    card = Set('Card')
+
+class Player(db.Entity):
+    turn = Required(int, unique=True)
+    rol = Required(int)
+    loyalty = Required(str)
+    is_alive = Required(bool)
+    chat_enabled = Required(bool)
+    is_investigated = Required(bool)
+    current_minister = Set('Turn', reverse='current_minister')
+    current_director = Set('Turn', reverse='current_director')
+    last_minister = Set('Turn', reverse='last_minister')
+    last_director = Set('Turn', reverse='last_director')
+    candidate_minister = Set('Turn', reverse='candidate_minister')
+    candidate_director = Set('Turn', reverse='candidate_director')
+    game_in = Required('Game')
+    vote = Set('Player_vote')
+
+class Game(db.Entity):
+    name = Required(str, 40)
+    min_players = Required(int)
+    max_players = Required(int)
+    creation_date = Required(datetime)
+    state = Required(int)
+    player = Set('Player')
+    turn = Set('Turn')
+    card = Set('Card')
+
+class Vote(db.Entity):
+    result = Required(bool)
+    turn = Required('Turn')
+    player_vote = Set('Player_vote')
+
+class Player_vote(db.Entity):
+    player = Required('Player')
+    vote = Required('Vote')
+    is_lumos = Required(bool)
+    PrimaryKey(vote, player)
+
+class Card(db.Entity):
+    order = Required(int, auto=True)
+    type = Required(int)
+    turn = Required('Turn')
+    game = Required('Game')
+    PrimaryKey(order, game)
 #
 
 db.generate_mapping(create_tables=True)
