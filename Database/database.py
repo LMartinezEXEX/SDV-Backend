f676f7e436bdcc569d17dfba7ebccbb7eae52b41
reydc <daniel.rey.consultas@gmail.com>
2020-10-23 13:29:08 -0300

    Modificado User

--- Database/database.py
+++ Database/database.py
@@ -11,13 +11,14 @@ db.bind(provider='sqlite', filename='secretVoldemort.sqlite', create_db=True)
 
 class User(db.Entity):
     email    = PrimaryKey(str, 100)
-    username = Required(str, 20, unique = True)
+    username = Required(str, 35, unique = True)
     password = Required(bytes)
     icon     = Required(bytes)
     creation_date    = Required(datetime.datetime)
     last_access_date = Required(datetime.datetime)
     is_validated     = Required(bool)
-    active           = Required(bool)
+    refresh_token    = Required(str)
+    refresh_token_expires = Required(datetime.datetime)
 
 #
 
