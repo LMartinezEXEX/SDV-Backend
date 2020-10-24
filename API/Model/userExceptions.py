6b9abecadfb095633d66325aa1c8cc0e1100c7b7
reydc <daniel.rey.consultas@gmail.com>
2020-10-23 21:30:29 -0300

    Añadida excepción

--- API/Model/userExceptions.py
+++ API/Model/userExceptions.py
@@ -1,5 +1,10 @@
 from fastapi import HTTPException, status
 
+not_found_exception = HTTPException(
+    status_code = status.HTTP_404_NOT_FOUND,
+    detail      = "Resource not found"
+)
+
 register_exception = HTTPException(
     status_code = status.HTTP_409_CONFLICT, 
     detail      = "Could not register the user"
@@ -8,6 +13,11 @@ register_exception = HTTPException(
 update_exception = HTTPException(
     status_code = status.HTTP_400_BAD_REQUEST,
     detail      = "Failed to update"
+)
+
+update_icon_exception = HTTPException(
+    status_code = status.HTTP_400_BAD_REQUEST,
+    detail      = "Failed to update icon. Formats allowed: jpeg, png, bmp, webp"
 )
 
 unauthorized_exception = HTTPException(
@@ -29,5 +39,5 @@ credentials_exception = HTTPException(
 
 profile_exception = HTTPException(
     status_code = status.HTTP_400_BAD_REQUEST,
-    detail      = "Login to see user profile"
+    detail      = "Login to see private profile"
 )
