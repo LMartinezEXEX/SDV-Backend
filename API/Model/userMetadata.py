13b96c3437eccf45ab04ca7efd4b8a04d5605d00
reydc <daniel.rey.consultas@gmail.com>
2020-10-23 21:31:11 -0300

    Metadata nueva para los endpoints públicos

--- API/Model/userMetadata.py
+++ API/Model/userMetadata.py
@@ -1,39 +1,40 @@
 # Metadata tags
 
-user_metadata_testing = [
-    {
-        "name": "User data (Testing)",
-        "description": "Get user data by email.",
-    },
-]
-
 user_metadata = [
+    {
+        "name": "User public data",
+        "description": "Return user public data",
+    },
+    {
+        "name": "User icon",
+        "description": "Return user icon",
+    },
     {
         "name": "Register",
         "description": "Register user.",
     },
     {
         "name": "Login",
-        "description": "Login endpoint. Returns token and session data for requests.",
+        "description": "Login endpoint: returns token and session data for requests.",
     },
     {
         "name": "Logout",
-        "description": "Logout endpoint. Revoke tokens and end session.",
+        "description": "Logout endpoint: revoke tokens and end session.",
     },
     {
-        "name": "Profile",
+        "name": "Private profile",
         "description": "Return user profile data",
     },
     {
         "name": "Update username",
-        "description": "Update username. Requires authentication",
+        "description": "Update username: requires authentication",
     },
     {
         "name": "Update password",
-        "description": "Update password. Requires authentication",
+        "description": "Update password: requires authentication",
     },
     {
         "name": "Update icon",
-        "description": "Update icon. Requires authentication",
+        "description": "Update icon: requires authentication",
     },
 ]
