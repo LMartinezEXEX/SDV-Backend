9e4da8a166e7f235e2918298e43251f48b6f3a26
reydc <daniel.rey.consultas@gmail.com>
2020-10-29 16:36:56 -0300

    Procesados para respetar la convenciones PEP8

--- API/Model/userModel.py
+++ API/Model/userModel.py
@@ -1,12 +1,12 @@
 # Imports
 import json
-from secrets      import token_hex
-from typing       import Dict, Any, Optional, Tuple
-from datetime     import datetime, timedelta
+from secrets import token_hex
+from typing import Dict, Any, Optional, Tuple
+from datetime import datetime, timedelta
 from http.cookies import SimpleCookie
 from pydantic import BaseModel, Field, EmailStr, validator
-from fastapi  import Cookie, Header, Depends, Request, Response, status
-from fastapi.encoders       import jsonable_encoder
+from fastapi import Cookie, Header, Depends, Request, Response, status
+from fastapi.encoders import jsonable_encoder
 from jose import JWTError, ExpiredSignatureError, jwt
 
 # Exceptions
@@ -14,7 +14,7 @@ from API.Model.userExceptions import credentials_exception, not_authenticated_ex
     unauthorized_exception, profile_exception, not_found_exception
 # Data for user management
 from API.Model.authData import SECRET_KEY, ALGORITHM, TOKEN_SEP, DOMAIN,\
-     ACCESS_TOKEN_EXPIRES_MINUTES, REFRESH_TOKEN_EXPIRES_MINUTES, EXPIRES_REFRESH, REFRESH_TOKEN_LENGTH
+    ACCESS_TOKEN_EXPIRES_MINUTES, REFRESH_TOKEN_EXPIRES_MINUTES, EXPIRES_REFRESH, REFRESH_TOKEN_LENGTH
 from USER_URLS import USER_LOGIN_URL
 # Security scheme
 from API.Model.securityScheme import OAuth2PasswordBearerWithCookie
@@ -22,20 +22,22 @@ from API.Model.securityScheme import OAuth2PasswordBearerWithCookie
 import Database.user_functions
 
 # Actual security scheme
-oauth2_scheme = OAuth2PasswordBearerWithCookie(tokenUrl = USER_LOGIN_URL)
+oauth2_scheme = OAuth2PasswordBearerWithCookie(tokenUrl=USER_LOGIN_URL)
 
 # User models
+
+
 class User(BaseModel):
     email: EmailStr
-    username: str = Field(min_length = 8, max_length = 35)
-    password: str = Field(min_length = 8, max_length = 50)
+    username: str = Field(min_length=8, max_length=35)
+    password: str = Field(min_length=8, max_length=50)
     icon: bytes
     creation_date: datetime
     last_access_date: datetime
     is_validated: bool
     refresh_token: str
     refresh_token_expires: datetime
-    
+
     @validator("email")
     def email_size(cls, val):
         if len(val) > 100:
@@ -44,76 +46,91 @@ class User(BaseModel):
             raise ValueError("Length of email is less tan 10")
         return val
 
-    class Config:
-        orm_mode = True
 
 class UserRegisterIn(BaseModel):
     email: EmailStr
     username: str
     password: str
 
+
 class UserProfile(BaseModel):
     email: EmailStr
     username: str
     is_validated: bool
 
+
 class UserProfileExtended(UserProfile):
     refresh_token: str
     refresh_token_expires: datetime
+
 
 class UserUpdateUsername(BaseModel):
     email: EmailStr
     new_username: str
     password: str
 
+
 class UserUpdatePassword(BaseModel):
     email: EmailStr
     old_password: str
     new_password: str
 
+
 class UserUpdateIcon(BaseModel):
     email: EmailStr
     password: str
 
+
 async def get_user_profile_by_email(email: EmailStr):
     user = Database.user_functions.get_user_by_email(email)
     return UserProfile(
-        email    = user.email,
-        username = user.username,
-        is_validated = user.is_validated
+        email=user.email,
+        username=user.username,
+        is_validated=user.is_validated
     )
+
 
 async def get_user_icon_by_email(email: EmailStr):
     user = Database.user_functions.get_user_by_email(email)
     return user.icon
 
+
 async def register(new_user: UserRegisterIn):
     Database.user_functions.register_user(
         User(
-            email    = new_user.email,
-            username = new_user.username,
-            password = new_user.password,
-            icon     = "".encode(),
-            creation_date    = datetime.utcnow(),
-            last_access_date = datetime.utcnow(),
-            is_validated  = False,
-            refresh_token = "empty",
-            refresh_token_expires = datetime(year = 1970, month = 1, day = 1, hour = 0, minute = 0, second = 0, microsecond = 0)
+            email=new_user.email,
+            username=new_user.username,
+            password=new_user.password,
+            icon="".encode(),
+            creation_date=datetime.utcnow(),
+            last_access_date=datetime.utcnow(),
+            is_validated=False,
+            refresh_token="empty",
+            refresh_token_expires=datetime(
+                year=1970,
+                month=1,
+                day=1,
+                hour=0,
+                minute=0,
+                second=0,
+                microsecond=0)
         )
     )
+
 
 async def get_user_auth(email: EmailStr):
     user = Database.user_functions.get_user_by_email(email)
     if user:
         return UserProfileExtended(
-            email    = user.email,
-            username = user.username,
-            is_validated  = user.is_validated,
-            refresh_token = user.refresh_token,
-            refresh_token_expires = user.refresh_token_expires
+            email=user.email,
+            username=user.username,
+            is_validated=user.is_validated,
+            refresh_token=user.refresh_token,
+            refresh_token_expires=user.refresh_token_expires
         )
     else:
         raise not_found_exception
+
 
 async def authenticate(email: EmailStr, password: str):
     if Database.user_functions.auth_user_password(email, password):
@@ -122,31 +139,39 @@ async def authenticate(email: EmailStr, password: str):
         # 1) User is not logged in
         # 2) Browser deleted expired cookies but application doesn't even know about it,
         #    then check out if we have a possible expired session
-        if user.refresh_token == "empty" or (user.refresh_token != "empty" and user.refresh_token_expires < datetime.utcnow()):
+        if user.refresh_token == "empty" or (
+                user.refresh_token != "empty" and user.refresh_token_expires < datetime.utcnow()):
             access_token = await new_access_token(
-                data = { "sub": user.email }, expires_delta = timedelta(minutes = ACCESS_TOKEN_EXPIRES_MINUTES)
+                data={"sub": user.email}, expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRES_MINUTES)
             )
             access_token_json = jsonable_encoder(access_token)
 
             refresh_token_value = token_hex(REFRESH_TOKEN_LENGTH)
-            
-            last_access_date = datetime.utcnow()
-            
-            refresh_token_expires = last_access_date + timedelta(minutes = REFRESH_TOKEN_EXPIRES_MINUTES)
 
-            Database.user_functions.activate_user(user.email, refresh_token_value, refresh_token_expires, last_access_date)
-            
+            last_access_date = datetime.utcnow()
+
+            refresh_token_expires = last_access_date + \
+                timedelta(minutes=REFRESH_TOKEN_EXPIRES_MINUTES)
+
+            Database.user_functions.activate_user(
+                user.email,
+                refresh_token_value,
+                refresh_token_expires,
+                last_access_date)
+
             response = Response(
-                status_code = status.HTTP_302_FOUND,
-                headers = {"Location": DOMAIN + "/", "WWW-Authenticate": "Bearer"}
+                status_code=status.HTTP_302_FOUND,
+                headers={
+                    "Location": DOMAIN + "/",
+                    "WWW-Authenticate": "Bearer"}
             )
             response.set_cookie(
-                key = "Authorization",
-                value    = f"Bearer {access_token_json} {TOKEN_SEP} Refresh {refresh_token_value}",
-                domain   = DOMAIN,
-                httponly = True,
-                max_age  = EXPIRES_REFRESH,
-                expires  = EXPIRES_REFRESH,
+                key="Authorization",
+                value=f"Bearer {access_token_json} {TOKEN_SEP} Refresh {refresh_token_value}",
+                domain=DOMAIN,
+                httponly=True,
+                max_age=EXPIRES_REFRESH,
+                expires=EXPIRES_REFRESH,
             )
 
             return response
@@ -155,25 +180,36 @@ async def authenticate(email: EmailStr, password: str):
     else:
         raise unauthorized_exception
 
+
 async def deauthenticate(email: EmailStr, refresh_token: str):
     user = await get_user_auth(email)
-    # Logout user by request if refresh token is the same, and refresh token expires in the future
+    # Logout user by request if refresh token is the same, and refresh token
+    # expires in the future
     if user.refresh_token != "empty" and user.refresh_token == refresh_token and user.refresh_token_expires > datetime.utcnow():
         Database.user_functions.deactivate_user(email)
     else:
         raise not_authenticated_exception
+
 
 async def new_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes = ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({ "exp": expire })
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm = ALGORITHM)
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-async def get_this_user(request: Request, cookie: str = Header(...), tokens: Tuple[str, str] = Depends(oauth2_scheme)):
+""" This function acts as a proxy, prepares request to be given to continue user
+    session or shuts it down. Caller must decide if it discards the response or 
+    if it should be modified
+"""
+
+async def get_this_user(
+    request: Request,
+    cookie: str = Header(...),
+    tokens: Tuple[str, str] = Depends(oauth2_scheme)):
     cookie_authorization_value = ""
     cookie_max_age = ""
     cookie_expires = ""
@@ -185,13 +221,14 @@ async def get_this_user(request: Request, cookie: str = Header(...), tokens: Tup
         cookie_max_age = v["max-age"]
         cookie_expires = v["expires"]
 
-    access_token  = tokens[0]
+    access_token = tokens[0]
     refresh_token = tokens[1]
     try:
-        # This is the case of user being logged and checks are fine,ie no exceptions
-        payload     = jwt.decode(access_token, SECRET_KEY, algorithms = [ALGORITHM])
+        # This is the case of user being logged and checks are fine,ie no
+        # exceptions
+        payload = jwt.decode(access_token, SECRET_KEY, algorithms=[ALGORITHM])
         token_email = payload.get("sub")
-        
+
         if token_email is None:
             raise credentials_exception
         user = await get_user_auth(token_email)
@@ -200,23 +237,28 @@ async def get_this_user(request: Request, cookie: str = Header(...), tokens: Tup
         if user.refresh_token == "empty" or user.refresh_token != refresh_token:
             raise profile_exception
         response = Response(
-            content = json.dumps(user.__dict__, default = str).encode(),
-            status_code = status.HTTP_200_OK,
-            headers = {"WWW-Authenticate": "Bearer"}
+            content=json.dumps(user.__dict__, default=str).encode(),
+            status_code=status.HTTP_200_OK,
+            headers={"WWW-Authenticate": "Bearer"}
         )
         response.set_cookie(
-            key = "Authorization",
-            value    = cookie_authorization_value,
-            domain   = DOMAIN,
-            httponly = True,
-            max_age  = cookie_max_age,
-            expires  = cookie_expires,
+            key="Authorization",
+            value=cookie_authorization_value,
+            domain=DOMAIN,
+            httponly=True,
+            max_age=cookie_max_age,
+            expires=cookie_expires,
         )
         return response
     except ExpiredSignatureError:
         # This is the case where signature expired
         # Signature expired, but we need to know if refresh expired too
-        payload = jwt.decode(access_token, SECRET_KEY, algorithms = [ALGORITHM], options = { "verify_exp": False })
+        payload = jwt.decode(
+            access_token,
+            SECRET_KEY,
+            algorithms=[ALGORITHM],
+            options={
+                "verify_exp": False})
         token_email = payload.get("sub")
 
         if token_email is None:
@@ -224,36 +266,39 @@ async def get_this_user(request: Request, cookie: str = Header(...), tokens: Tup
         user = await get_user_auth(token_email)
         if user is None:
             raise credentials_exception
-        
+
         if user.refresh_token != "empty" and user.refresh_token == refresh_token and user.refresh_token_expires > datetime.utcnow():
-                # Refresh token corresponds to user and
-                # Bearer token expired but Refresh token didn't, so issue new tokens
-                access_token = await new_access_token(
-                    data = { "sub": user.email }, expires_delta = timedelta(minutes = ACCESS_TOKEN_EXPIRES_MINUTES)
-                )
-                access_token_json = jsonable_encoder(access_token)
+            # Refresh token corresponds to user and
+            # Bearer token expired but Refresh token didn't, so issue new
+            # tokens
+            access_token = await new_access_token(
+                data={"sub": user.email}, expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRES_MINUTES)
+            )
+            access_token_json = jsonable_encoder(access_token)
 
-                refresh_token_value = token_hex(REFRESH_TOKEN_LENGTH)
+            refresh_token_value = token_hex(REFRESH_TOKEN_LENGTH)
 
-                refresh_token_expires = datetime.utcnow() + timedelta(minutes = REFRESH_TOKEN_EXPIRES_MINUTES)
+            refresh_token_expires = datetime.utcnow(
+            ) + timedelta(minutes=REFRESH_TOKEN_EXPIRES_MINUTES)
 
-                Database.user_functions.activate_user(user.email, refresh_token_value, refresh_token_expires)
-                
-                user = await get_user_auth(user.email)
-                response = Response(
-                    content = json.dumps(user.__dict__, default = str).encode,
-                    status_code = status.HTTP_200_OK,
-                    headers = {"WWW-Authenticate": "Bearer"}
-                )
-                response.set_cookie(
-                    key = "Authorization",
-                    value    = f"Bearer {access_token_json} {TOKEN_SEP} Refresh {refresh_token_value}",
-                    domain   = DOMAIN,
-                    httponly = True,
-                    max_age  = EXPIRES_REFRESH,
-                    expires  = EXPIRES_REFRESH,
-                )
-                return response
+            Database.user_functions.activate_user(
+                user.email, refresh_token_value, refresh_token_expires)
+
+            user = await get_user_auth(user.email)
+            response = Response(
+                content=json.dumps(user.__dict__, default=str).encode,
+                status_code=status.HTTP_200_OK,
+                headers={"WWW-Authenticate": "Bearer"}
+            )
+            response.set_cookie(
+                key="Authorization",
+                value=f"Bearer {access_token_json} {TOKEN_SEP} Refresh {refresh_token_value}",
+                domain=DOMAIN,
+                httponly=True,
+                max_age=EXPIRES_REFRESH,
+                expires=EXPIRES_REFRESH,
+            )
+            return response
         else:
             # Refresh token is not tracked, then token is being replayed somehow
             # Or Refresh token is not equal to the stored one
@@ -264,19 +309,27 @@ async def get_this_user(request: Request, cookie: str = Header(...), tokens: Tup
         # Any other exception
         raise credentials_exception
 
+
 async def change_username(update_data: UserUpdateUsername):
-    if Database.user_functions.auth_user_password(update_data.email, update_data.password):
-        return update_data.new_username == Database.user_functions.change_username(update_data)
+    if Database.user_functions.auth_user_password(
+            update_data.email, update_data.password):
+        return update_data.new_username == Database.user_functions.change_username(
+            update_data)
     else:
         return False
 
+
 async def change_password(update_data: UserUpdatePassword):
-    if update_data.old_password != update_data.new_password and Database.user_functions.auth_user_password(update_data.email, update_data.old_password):
+    if update_data.old_password != update_data.new_password and Database.user_functions.auth_user_password(
+            update_data.email, update_data.old_password):
         return Database.user_functions.change_password(update_data)
     return False
 
+
 async def change_icon(update_data: UserUpdateIcon, new_icon: bytes):
-    if Database.user_functions.auth_user_password(update_data.email, update_data.password):
-        return new_icon == Database.user_functions.change_icon(update_data, new_icon)
+    if Database.user_functions.auth_user_password(
+            update_data.email, update_data.password):
+        return new_icon == Database.user_functions.change_icon(
+            update_data, new_icon)
     else:
         return False
--- main.py
+++ main.py
@@ -1,50 +1,50 @@
 # Imports
 import json
-from imghdr       import what
-from itertools    import chain
-from datetime     import datetime, timedelta
-from typing       import Optional
+from imghdr import what
+from itertools import chain
+from datetime import datetime, timedelta
+from typing import Optional
 from http.cookies import SimpleCookie
-from pydantic     import EmailStr
-from fastapi      import FastAPI, Cookie, Depends, Form, File, UploadFile, Response, status
+from pydantic import EmailStr
+from fastapi import FastAPI, Cookie, Depends, Form, File, UploadFile, Response, status
 from fastapi.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
 
-from fastapi.security   import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm
 from API.Model.authData import DOMAIN, TOKEN_SEP
 from USER_URLS import USER_REGISTER_URL, USER_LOGIN_URL, USER_LOGOUT_URL, USER_PUBLIC_PROFILE_URL,\
-     USER_PRIVATE_PROFILE_URL, USER_ICON_URL, USER_UPDATE_USERNAME_URL, USER_UPDATE_PASSWORD_URL, USER_UPDATE_ICON_URL
+    USER_PRIVATE_PROFILE_URL, USER_ICON_URL, USER_UPDATE_USERNAME_URL, USER_UPDATE_PASSWORD_URL, USER_UPDATE_ICON_URL
 
 # User Model
 from API.Model.userModel import UserRegisterIn, UserProfileExtended,\
-     UserUpdateUsername, UserUpdatePassword, UserUpdateIcon,\
-     get_user_profile_by_email, get_user_icon_by_email, register,\
-     authenticate, deauthenticate, get_this_user,\
-     change_username, change_password, change_icon
+    UserUpdateUsername, UserUpdatePassword, UserUpdateIcon,\
+    get_user_profile_by_email, get_user_icon_by_email, register,\
+    authenticate, deauthenticate, get_this_user,\
+    change_username, change_password, change_icon
 
 from API.Model.userExceptions import not_found_exception, credentials_exception,\
-     profile_exception, register_exception, update_exception, update_icon_exception
+    profile_exception, register_exception, update_exception, update_icon_exception
 
 from API.Model.userMetadata import user_metadata
 
 # Add metadata tags for each module
 tags_metadata = list(
     chain.from_iterable([
-        [ { "name": "Root", "description": "", } ],
+        [{"name": "Root", "description": "", }],
         user_metadata,
-        ]
+    ]
     )
 )
 
 app = FastAPI(
-    title       = "Secret Voldemort API",
-    description = "Secret Voldemort is a multiplayer game based on Secret Hitler where two teams compete against each other for the control of Hogwarts.",
-    version     = "0.0.0",
-    openapi_url  = "/api/openapi.json",
-    docs_url     = "/api/docs",
-    redoc_url    = "/api/redoc",
-    openapi_tags = tags_metadata,
-    debug = True
+    title="Secret Voldemort API",
+    description="Secret Voldemort is a multiplayer game based on Secret Hitler where two teams compete against each other for the control of Hogwarts.",
+    version="0.0.0",
+    openapi_url="/api/openapi.json",
+    docs_url="/api/docs",
+    redoc_url="/api/redoc",
+    openapi_tags=tags_metadata,
+    debug=True
 )
 
 origins = [
@@ -53,86 +53,86 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = origins,
-    allow_credentials = True,
-    allow_methods = ["*"],
-    allow_headers = ["*"],
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-# Root
+
 @app.get(
     "/",
-    tags = ["Root"]
-    )
+    tags=["Root"]
+)
 async def get_root():
     return "Secret Voldemort API"
 
-# Register
+
 @app.post(
     USER_REGISTER_URL,
-    status_code = status.HTTP_201_CREATED,
-    tags = ["Register"]
+    status_code=status.HTTP_201_CREATED,
+    tags=["Register"]
 )
 async def user_register(new_user: UserRegisterIn):
     try:
         await register(new_user)
-    except:
+    except BaseException:
         raise register_exception
     return {
-        "username": new_user.username,
-        "operation_result": "success"
+        "email": new_user.email,
+        "result": "success"
     }
 
-# Login
+
 @app.post(
     USER_LOGIN_URL,
-    status_code = status.HTTP_302_FOUND,
-    tags = ["Login"]
+    status_code=status.HTTP_302_FOUND,
+    tags=["Login"]
 )
 async def user_login(form_data: OAuth2PasswordRequestForm = Depends()):
     # Spec requires the field username, but there we store the email
-    email    = form_data.username
+    email = form_data.username
     password = form_data.password
     return await authenticate(email, password)
 
-# Get public profile by email
+
 @app.get(
     USER_PUBLIC_PROFILE_URL,
-    status_code = status.HTTP_200_OK,
-    tags = ["User public data"]
+    status_code=status.HTTP_200_OK,
+    tags=["User public data"]
 )
 async def get_user_public_profile(email: EmailStr):
     try:
         return await get_user_profile_by_email(email)
-    except:
+    except BaseException:
         raise not_found_exception
 
-# Get icon by email
+
 @app.get(
     USER_ICON_URL,
-    status_code = status.HTTP_200_OK,
-    tags = ["User icon"]
+    status_code=status.HTTP_200_OK,
+    tags=["User icon"]
 )
 async def get_user_icon(email: EmailStr):
     try:
         return Response(await get_user_icon_by_email(email))
-    except:
+    except BaseException:
         raise not_found_exception
 
-# Profile
+
 @app.get(
     USER_PRIVATE_PROFILE_URL,
-    status_code = status.HTTP_200_OK,
-    tags = ["Private profile"]
+    status_code=status.HTTP_200_OK,
+    tags=["Private profile"]
 )
 async def profile(Authorization: str = Cookie(...), response: Response = Depends(get_this_user)):
     return response
 
-# Logout
+
 @app.post(
     USER_LOGOUT_URL,
-    status_code = status.HTTP_302_FOUND,
-    tags = ["Logout"]
+    status_code=status.HTTP_302_FOUND,
+    tags=["Logout"]
 )
 async def user_logout(Authorization: str = Cookie(...), response: Response = Depends(get_this_user)):
     user = UserProfileExtended(**json.loads(response.body.decode()))
@@ -143,81 +143,83 @@ async def user_logout(Authorization: str = Cookie(...), response: Response = Dep
     refresh_splitted = refresh.split(" ")
     if len(refresh_splitted) != 2:
         raise not_authenticated_exception
-    refresh_token  = refresh_splitted[1]
+    refresh_token = refresh_splitted[1]
     await deauthenticate(user.email, refresh_token)
     response = Response(
-        content = json.dumps({
-            "username": user.username,
-            "operation_result": "success"
+        content=json.dumps({
+            "email": user.email,
+            "result": "success"
         }).encode(),
-        status_code = status.HTTP_302_FOUND,
-        headers = { "Location": DOMAIN + "/" }
+        status_code=status.HTTP_302_FOUND,
+        headers={"Location": DOMAIN + "/"}
     )
-    response.delete_cookie("Authorization", domain = DOMAIN)
+    response.delete_cookie("Authorization", domain=DOMAIN)
     return response
 
-# Update username
+
 @app.put(
     USER_UPDATE_USERNAME_URL,
-    status_code = status.HTTP_200_OK,
-    tags = ["Update username"]
+    status_code=status.HTTP_200_OK,
+    tags=["Update username"]
 )
 async def user_update_username(
-    update_data: UserUpdateUsername, 
-    Authorization: str = Cookie(...), response: Response = Depends(get_this_user)):
+        update_data: UserUpdateUsername,
+        Authorization: str = Cookie(...), response: Response = Depends(get_this_user)):
     user = UserProfileExtended(**json.loads(response.body.decode()))
     if update_data.email == user.email and (await change_username(update_data)):
         response.body = json.dumps({
-            "username": update_data.new_username,
-            "operation_result": "success"
+            "email": update_data.email,
+            "result": "success"
         }).encode()
         return response
     else:
         raise update_exception
 
-# Update password
+
 @app.put(
     USER_UPDATE_PASSWORD_URL,
-    status_code = status.HTTP_200_OK,
-    tags = ["Update password"]
+    status_code=status.HTTP_200_OK,
+    tags=["Update password"]
 )
 async def user_update_password(
-    update_data: UserUpdatePassword, 
-    Authorization: str = Cookie(...), response: Response = Depends(get_this_user)):
+        update_data: UserUpdatePassword,
+        Authorization: str = Cookie(...), response: Response = Depends(get_this_user)):
     user = UserProfileExtended(**json.loads(response.body.decode()))
     if update_data.email == user.email and (await change_password(update_data)):
         response.body = json.dumps({
-            "username": user.username,
-            "operation_result": "success"
+            "email": user.email,
+            "result": "success"
         }).encode()
         return response
     else:
         raise update_exception
 
-# Update icon
+
 @app.put(
     USER_UPDATE_ICON_URL,
-    status_code = status.HTTP_200_OK,
-    tags = ["Update icon"]
+    status_code=status.HTTP_200_OK,
+    tags=["Update icon"]
 )
 async def user_update_icon(
-    email: EmailStr = Form(...), password: str = Form(...), new_icon: UploadFile = File(...),
-    Authorization: str = Cookie(...), response: Response = Depends(get_this_user)):
+        email: EmailStr = Form(...), password: str = Form(...), new_icon: UploadFile = File(...),
+        Authorization: str = Cookie(...), response: Response = Depends(get_this_user)):
     user = UserProfileExtended(**json.loads(response.body.decode()))
-    update_data = UserUpdateIcon(email = email, password = password)
+    update_data = UserUpdateIcon(email=email, password=password)
 
-    if new_icon.content_type not in ["image/jpeg", "image/png", "image/bmp", "image/webp"]:
+    if new_icon.content_type not in [
+            "image/jpeg", "image/png", "image/bmp", "image/webp"]:
         raise update_icon_exception
 
     raw_icon = new_icon.file.read()
 
-    if what(new_icon.filename ,h = raw_icon) not in ["jpeg", "png", "bmp", "webp"]:
+    if what(new_icon.filename, h=raw_icon) not in [
+            "jpeg", "png", "bmp", "webp"]:
         raise update_icon_exception
-    
+
     if update_data.email == user.email and (await change_icon(update_data, raw_icon)):
         response.body = json.dumps({
-            "username": user.username,
-            "operation_result": "success"
+            "email": user.email,
+            "result": "success"
         }).encode()
         return response
     else:
