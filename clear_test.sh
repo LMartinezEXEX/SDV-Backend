f7c3048bc8bd8445a9fe51315dc0d1996bb8ec76
reydc <daniel.rey.consultas@gmail.com>
2020-11-18 18:11:16 -0300

    Limpiar pycache y .sqlite, no corre más tests

---
+++ clear_test.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Borrar los archivos que no hacen falta y testear seguidamente
+
+echo "Cleaning..."
+
+if [ -d "./__pycache__" ]
+then
+    rm -rd "./__pycache__"
+fi
+
+if [ -d "./API/Model/__pycache__" ]
+then
+    rm -rd "./API/Model/__pycache__"
+fi
+
+if [ -d "./API/TestAPI/TestUser/__pycache__" ]
+then
+    rm -rd "./API/TestAPI/TestUser/__pycache__"
+fi
+
+if [ -d "./Database/__pycache__" ]
+then
+    rm -rd "./Database/__pycache__"
+fi
+
+if [ -e "./Database/*.sqlite" ]
+then
+    rm "./Database/*.sqlite"
+fi
+
+echo "Sleep for 2 seconds..."
+sleep 2
+
+echo ""
+echo -e "\e[32m\e[1mDone"
